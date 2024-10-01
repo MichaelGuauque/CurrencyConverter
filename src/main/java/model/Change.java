f12733ed9a1cf728dd460971968e4c30b753cd5e
MichaelGuauque <mcalejo98@gmail.com>
2024-09-30 20:05:38 -0500

    Creacion clase

---
+++ src/main/java/model/Change.java
@@ -0,0 +1,59 @@
+package model;
+
+public class Change {
+
+    private String monedaBase;
+    private String monedaCambiar;
+    private double montoAConvertir;
+    private double conversion;
+
+    public Change() {
+    }
+
+    public Change(String monedaBase, String monedaCambiar, double montoAConvertir, double conversion) {
+        this.monedaBase = monedaBase;
+        this.monedaCambiar = monedaCambiar;
+        this.montoAConvertir = montoAConvertir;
+        this.conversion = conversion;
+    }
+
+    public String getMonedaBase() {
+        return monedaBase;
+    }
+
+    public void setMonedaBase(String monedaBase) {
+        this.monedaBase = monedaBase;
+    }
+
+    public String getMonedaCambiar() {
+        return monedaCambiar;
+    }
+
+    public void setMonedaCambiar(String monedaCambiar) {
+        this.monedaCambiar = monedaCambiar;
+    }
+
+    public double getMontoAConvertir() {
+        return montoAConvertir;
+    }
+
+    public void setMontoAConvertir(double montoAConvertir) {
+        this.montoAConvertir = montoAConvertir;
+    }
+
+    public double getConversion() {
+        return conversion;
+    }
+
+    public void setConversion(double conversion) {
+        this.conversion = conversion;
+    }
+
+    @Override
+    public String toString() {
+        return  "Moneda Base = '" + monedaBase + '\'' +
+                ", Moneda a Cambiar = '" + monedaCambiar + '\'' +
+                ", Monto A Convertir = " + montoAConvertir +
+                ", Conversion = " + conversion;
+    }
+}
